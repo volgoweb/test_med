9fefb9b3b7da091c2c901c18fd3afe19d35dfc23
volgoweb <volgoweb@bk.ru>
2015-09-22 17:44:47 +0300

    правки install

--- test_med/install.sh
+++ test_med/install.sh
@@ -2,7 +2,7 @@
 sudo apt-get install python-virtualenv
 virtualenv --no-site-packages --distribute -p /usr/bin/python3.4 venv
 source venv/bin/activate
-pip install -r test_med/app/requirements.txt
-python test_med/manage.py syncdb --noinput
-python test_med/manage.py migrate
-python test_med/manage.py loaddata demo_accounts.json
+pip install -r app/requirements.txt
+python manage.py syncdb --noinput
+python manage.py migrate
+python manage.py loaddata demo_accounts.json
