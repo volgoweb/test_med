1857157e37a5069b626681f2d1ba4a7184dc2aaf
volgoweb <volgoweb@bk.ru>
2015-09-23 11:50:33 +0300

    поправил файл установки

--- test_med/start.sh
+++ test_med/start.sh
@@ -6,3 +6,6 @@ pip install -r app/requirements.txt
 python manage.py syncdb --noinput
 python manage.py migrate
 python manage.py loaddata demo_accounts.json
+python manage.py runserver 0.0.0.0:80 &
+echo "Install has been completed. Host: 0.0.0.0:80"
+wait
